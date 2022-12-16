59093db0f9199f5051822656e39e33c86f67c795
ayberkderingoz <ayberkderingoz@gmail.com>
2022-12-16 22:20:35 +0300

    Firebase Update

--- throw_a_party/lib/main.dart
+++ throw_a_party/lib/main.dart
@@ -5,11 +5,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  runApp(const MyApp());
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
 }
+
 /*void main() {
   runApp(const MyApp());
 }*/
