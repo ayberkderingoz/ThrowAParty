f776a05f725c11eb1e21094538f91922887db66a
eardage <62185950+eardage@users.noreply.github.com>
2022-12-17 02:11:23 +0300

    lobi bitti

--- throw_a_party/lib/screens/lobby_screen.dart
+++ throw_a_party/lib/screens/lobby_screen.dart
@@ -20,6 +20,119 @@ class LobbyScreen extends StatelessWidget {
         ),
       ),
       
-    );
+      backgroundColor: Color(0xFFEAEEF6),
+        body: SafeArea(
+            child: Center(
+                child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 30),
+            //sistem yöneticisi girişi
+            GradientText(
+              'Welcome!',
+              style: const TextStyle(
+                fontFamily: "Poppins",
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+              colors: const [
+                saris,
+                acikmorus,
+                pembis,
+                mavis,
+              ],
+            ),
+            SizedBox(height: 40),
+
+            
+
+            
+
+            //Lobi oluşturma butonu
+            Container(
+                width: 250,
+                height: 40,
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    stops: [
+                      0.1,
+                      0.4,
+                      0.6,
+                      0.9,
+                    ],
+                    colors: [
+                      mavis,
+                      acikmorus,
+                      pembis,
+                      saris,
+                    ],
+                  ),
+                  border: Border.all(color: Color(0xBFAA00)),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: ElevatedButton(
+                    onPressed: () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => LobbyScreen())),
+                    child: Text('Create a lobby',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontFamily: "Poppins")),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.transparent),
+                      shadowColor:
+                          MaterialStateProperty.all<Color>(Colors.deepPurple),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0))),
+                    ))),
+            SizedBox(height: 30),
+            //Lobiye katılma butonu
+            Container(
+                width: 250,
+                height: 40,
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    stops: [
+                      0.1,
+                      0.4,
+                      0.6,
+                      0.9,
+                    ],
+                    colors: [
+                      mavis,
+                      acikmorus,
+                      pembis,
+                      saris,
+                    ],
+                  ),
+                  border: Border.all(color: Color(0xBFAA00)),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: ElevatedButton(
+                    onPressed: () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => LobbyScreen())),
+                    child: Text('Join a lobby',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontFamily: "Poppins")),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.transparent),
+                      shadowColor:
+                          MaterialStateProperty.all<Color>(Colors.deepPurple),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0))),
+                    ))),
+            SizedBox(height: 30),
+          ],
+            ))));
   }
 }
