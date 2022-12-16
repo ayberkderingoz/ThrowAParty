b156b5729ad9479a6ac408d66b2fa18d098833f5
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-12-17 00:39:19 +0300

    bisiler sildim

--- throw_a_party/lib/components/animated_btn.dart
+++ throw_a_party/lib/components/animated_btn.dart
@@ -22,15 +22,11 @@ class AnimatedBtn extends StatelessWidget {
         width: 260,
         child: Stack(
           children: [
-            // Just a button no animation
-            // Let's fix that
             RiveAnimation.asset(
               "assets/RiveAssets/button.riv",
-              // Once we restart the app it shows the animation
               controllers: [_btnAnimationColtroller],
             ),
             Positioned.fill(
-              // But it's not center
               top: 8,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
