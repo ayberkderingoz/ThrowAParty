9dd4177d0a0c03c114f6a026254531ac2173f76a
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-12-16 22:19:11 +0300

    Update onboarding_screen.dart

--- throw_a_party/lib/screens/onboarding_screen.dart
+++ throw_a_party/lib/screens/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
@@ -33,22 +34,47 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             child: SizedBox(),
           )),
           SafeArea(
-              child: Column(
-            children: [
-              SizedBox(
-                width: 260,
-                child: Column(
-                  children: [
-                    Text("Plan perfect parties",
-                        style: TextStyle(
-                          fontSize: 60,
-                          fontFamily: "Poppins",
-                          height: 1.2,
-                        )),
-                  ],
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: 260,
+                  child: Column(
+                    children: [
+                      Text("Plan perfect parties",
+                          style: TextStyle(
+                            fontSize: 60,
+                            fontFamily: "Poppins",
+                            height: 1.2,
+                          )),
+                      Text(
+                          "UniParty is the perfect app for planning parties for you and your loved ones. It is designed by university students for university students."),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(
+                    height: 80,
+                    width: 260,
+                    child: Stack(children: [
+                      RiveAnimation.asset("assets/RiveAssets/button.riv"),
+                      Positioned.fill(
+                        top: 8,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(CupertinoIcons.arrow_right),
+                            SizedBox(width: 8),
+                            Text(
+                              "Start planning",
+                              style: TextStyle(fontWeight: FontWeight.w500),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ])),
+              ],
+            ),
           ))
         ],
       ),
