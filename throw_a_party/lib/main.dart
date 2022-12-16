18ea1147a55fb0eff394c1fde962e82a78207a5d
ayberkderingoz <ayberkderingoz@gmail.com>
2022-12-16 22:00:20 +0300

    Firebase Commit to main

--- throw_a_party/lib/main.dart
+++ throw_a_party/lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:throw_a_party/screens/onboarding_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+/*void main() {
+  runApp(const MyApp());
+}*/
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
