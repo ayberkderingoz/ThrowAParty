4116567283ed388afa3420d9bf7a6d1af56e4ddc
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-12-17 01:04:55 +0300

    benim degisiklikler

--- throw_a_party/lib/screens/login_screen.dart
+++ throw_a_party/lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:throw_a_party/screens/onboarding_screen.dart';
 import 'package:throw_a_party/constants.dart';
@@ -20,7 +21,6 @@ class LoginScreen extends StatelessWidget {
             Padding(
                 padding: EdgeInsets.only(right: 30.0),
                 child: IconButton(
-                  
                   onPressed: () {},
                   icon: Icon(Icons.language, color: Colors.yellow, size: 30),
                 )),
@@ -33,8 +33,7 @@ class LoginScreen extends StatelessWidget {
                         MaterialPageRoute(
                             builder: (context) => OnboardingScreen()));
                   },
-                  icon:
-                      Icon(Icons.dark_mode, color: pembis, size: 30),
+                  icon: Icon(Icons.dark_mode, color: pembis, size: 30),
                 ))
           ],
         ),
@@ -44,7 +43,30 @@ class LoginScreen extends StatelessWidget {
                 child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(Icons.admin_panel_settings, size: 70, color: Colors.blueGrey),
+            SizedBox(
+              width: 250.0,
+              height: 100,
+              child: DefaultTextStyle(
+                style: const TextStyle(
+                  fontSize: 35,
+                  color: visnemsi,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 7.0,
+                      color: acikpembis,
+                      offset: Offset(0, 0),
+                    ),
+                  ],
+                ),
+                child: AnimatedTextKit(
+                  repeatForever: true,
+                  animatedTexts: [
+                    FlickerAnimatedText('UniParty'),
+                  ],
+                  onTap: () {},
+                ),
+              ),
+            ),
             SizedBox(height: 30),
             //sistem yöneticisi girişi
             Text(
@@ -97,91 +119,86 @@ class LoginScreen extends StatelessWidget {
                 child: Container(
                   decoration: BoxDecoration(
                     gradient: LinearGradient(
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomLeft,
-                        stops: [
-                          0.1,
-                          0.4,
-                          0.6,
-                          0.9,
-                        ],
-                        colors: [
-                          mavis,
-                          acikmorus,
-                          pembis,
-                          saris,
-                        ],
-                      ),
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      stops: [
+                        0.1,
+                        0.4,
+                        0.6,
+                        0.9,
+                      ],
+                      colors: [
+                        mavis,
+                        acikmorus,
+                        pembis,
+                        saris,
+                      ],
+                    ),
                     border: Border.all(color: Color(0XBFAA00)),
                     borderRadius: BorderRadius.circular(20),
                   ),
                   child: Padding(
                       padding: const EdgeInsets.only(left: 20.0),
-                      
                       child: Container(
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
                             begin: Alignment.topRight,
-                        end: Alignment.bottomLeft,
-                        stops: [
-                          0.1,
-                          0.4,
-                          0.6,
-                          0.9,
-                        ],
-                        colors: [
-                          mavis,
-                          acikmorus,
-                          pembis,
-                          saris,
-                        ],
-                          )
-                        ),
-                        child: TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Şifre',
-                        ),
-                      )
-                      )
-                      ),
+                            end: Alignment.bottomLeft,
+                            stops: [
+                              0.1,
+                              0.4,
+                              0.6,
+                              0.9,
+                            ],
+                            colors: [
+                              mavis,
+                              acikmorus,
+                              pembis,
+                              saris,
+                            ],
+                          )),
+                          child: TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Şifre',
+                            ),
+                          ))),
                 )),
             SizedBox(height: 40),
 
             //giriş yap butonu
-            
-            
-             Container(
-              
+
+            Container(
                 width: 167,
                 height: 40,
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomLeft,
-                        stops: [
-                          0.1,
-                          0.4,
-                          0.6,
-                          0.9,
-                        ],
-                        colors: [
-                          mavis,
-                          acikmorus,
-                          pembis,
-                          saris,
-                        ],
-                      ),
-                      border: Border.all(color: Color(0xBFAA00)),
-                      borderRadius: BorderRadius.circular(20),
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    stops: [
+                      0.1,
+                      0.4,
+                      0.6,
+                      0.9,
+                    ],
+                    colors: [
+                      mavis,
+                      acikmorus,
+                      pembis,
+                      saris,
+                    ],
+                  ),
+                  border: Border.all(color: Color(0xBFAA00)),
+                  borderRadius: BorderRadius.circular(20),
                 ),
                 child: ElevatedButton(
                     onPressed: () {},
                     child: Text('Giriş yap',
                         style: TextStyle(color: Colors.white, fontSize: 15)),
                     style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.transparent),
                       shadowColor:
                           MaterialStateProperty.all<Color>(Colors.deepPurple),
                       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
